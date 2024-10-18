151dfb4f0b1b455b437503709cd246be3f4c269d
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-10-18 20:07:06 +0000

    Create POST request and create function to display data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 //Will call the modal defined in our HTML with Bootstrap (the syntax is described in BS documentation)
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-//Make our "Check key" button show us the expiry date of API_KEY
+//Make our "Check key" button connect to the function which will show us the expiry date of API_KEY
 document.getElementById("status"). addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
@@ -39,6 +39,69 @@ function displayStatus (data) {
 
     headingText.innerText = "API Key Status";
     bodyText.innerHTML = `<div>Your key is valid until</div> <div>${data.expiry}</div>`;
+
+    resultsModal.show();
+
+}
+
+/* Make our "Run Checks" button connect to the function which will post the user completed form and return
+errors found in their JS code */
+document.getElementById("submit"). addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    /* FormData is a JS interface which captures all the fields in an HTML form and returns them as an object
+    which we can then give to "fetch()" */
+    const form = new FormData(document.getElementById("checksform"));
+
+    // This for loop was a test to see if FormData was working (returning the form fields as an object)
+    /*for (let el of from.entries()) {
+        console.log(el); 
+    } */
+
+    /*This is our POST request, of the API URL, demonstrating authorisation with our API KEY. 
+    "body" is the main information we are sending and want a response to*/
+    const response = await fetch(API_URL, {
+                                            method: "POST",
+                                            headers: {
+                                                        "Authorization": API_KEY,
+                                                        },
+                                            body: form,
+                            });
+    const data = await response.json();
+
+    /* If no errors, carry out displayErrors function (which will use the data obtained from the API to 
+    tell our users if there are errors in their code) */
+    if (response.ok) {
+        displayErrors(data);
+    }
+    //If errors
+    else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors (data) {
+    //Heading of the modal which is called at the end of this function
+    let heading = `JSHints results for ${data.file}`;
+
+    //results will form the body of the modal
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    }
+    else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    const headingText = document.getElementById("resultsModalTitle");
+    const bodyText = document.getElementById("results-content");
+
+    headingText.innerText = heading;
+    bodyText.innerHTML = results;
 
     resultsModal.show();
 
